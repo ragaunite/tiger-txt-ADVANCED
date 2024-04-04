6964afcf1343d4f3766580c09766e1318c5bcfe1
ragaunite <164988955+ragaunite@users.noreply.github.com>
2024-04-04 19:09:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
-API_ID = API_ID = 22609670
+API_ID = API_ID = 29485859
 
-API_HASH = os.environ.get("API_HASH", "3506d8474ad1f4f5e79b7c52a5c3e88d")
+API_HASH = os.environ.get("API_HASH", "6628abb836flab2eaca4df8c514d291a")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6739442523:AAHHA5HZmU6sqI-elmhTs8dwHqfhTizWwnQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6709742997:AAEUGizNtIHYCtO-365Ao2IwOwCxczfH07E")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6981453498))
+OWNER = int(os.environ.get("OWNER", 6045786693))
 
-LOG = -1002070057679
+LOG = -1002140760823
 
 try:
-    ADMINS=[]
+    ADMINS=[6254332429]
     for x in (os.environ.get("ADMINS", "5987970971").split()):
         ADMINS.append(int(x))
 except ValueError:
