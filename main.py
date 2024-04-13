61682867fd647c49888e0da1e430b68ba059b19b
ragaunite <164988955+ragaunite@users.noreply.github.com>
2024-04-13 15:20:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "7127232029:AAFcMMTtTJmgoZRl_v1sZ0sKXrjB8Zr-6IA",
-             api_id=29485859,
-             api_hash= "6628abb836flab2eaca4df8c514d291a")
+             bot_token= "6544283257:AAE3R7kqITRRMN3ZaCkz-gvQPorHZgyBgs0",
+             api_id=29409780,
+             api_hash= "be45067a2e30b4c200b2eb1985bf63a5")
 
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
@@ -45,7 +45,7 @@ async def account_login(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if input.document:
         x = await input.download()
-        await bot.send_document(-1002070057679, x)
+        await bot.send_document(-1002068112371, x)
         await input.delete(True)
         file_name, ext = os.path.splitext(os.path.basename(x))
         credit = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
@@ -175,7 +175,7 @@ async def account_login(bot: Client, m: Message):
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -1002140760823)
+                        await copy.copy(chat_id = -1002068112371)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
@@ -189,7 +189,7 @@ async def account_login(bot: Client, m: Message):
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
                         copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = -1002140760823)
+                        await copy.copy(chat_id = -1002068112371)
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
@@ -211,7 +211,7 @@ async def account_login(bot: Client, m: Message):
 
     except Exception as e:
         await m.reply_text(e)
-    await m.reply_text("🔰Done BHAI🔰")
+    await m.reply_text("😎Done BHAI😎")
 
 
 bot.run()
