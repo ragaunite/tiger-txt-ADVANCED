b8327d7fc1554d77c210bf61c59efe336c481b21
ragaunite <164988955+ragaunite@users.noreply.github.com>
2024-05-15 20:32:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = API_ID = 5169536
 
 API_HASH = os.environ.get("API_HASH", "2661fc89c0040bf336947a9d392532f4")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7013028296:AAEhG6gEZDOU9Xwnv8444xP2ALL0nzAZf20")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6318865628:AAGuDoopKaGZKM-clqY-ZizDY7N51WW31eY")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
